65a98487f1bbd56dae49c34bf76b6c1d9c22aa64
GH4Bo <845455655@qq.com>
2026-04-29 00:11:56 +0800

    first python file

---
+++ main.py
@@ -0,0 +1,8 @@
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# import sklearn
+
+print("环境搭建成功！")
+# print(f"NumPy版本: {np.__version__}")
+# print(f"Pandas版本: {pd.__version__}")
